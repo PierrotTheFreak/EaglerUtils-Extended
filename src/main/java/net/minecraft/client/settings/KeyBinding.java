e1be504d5f85289bcf560fb4b6dd93c0534b9388
PierrotTheFreak <pierrotthefreak0@gmail.com>
2026-08-18 16:36:09 -0400

    Dispatch individual macro hotkeys through KeyBinding

--- src/main/java/net/minecraft/client/settings/KeyBinding.java
+++ src/main/java/net/minecraft/client/settings/KeyBinding.java
@@ -6,8 +6,8 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
 import java.util.function.Supplier;
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.uiutils.UiUtilsMacroKeyBinding;
+import net.minecraft.client.gui.uiutils.UiUtilsMacroManager;
 import net.minecraft.client.resources.I18n;
 import net.minecraft.client.util.InputMappings;
 import net.minecraft.util.Util;
@@ -19,14 +19,9 @@ public class KeyBinding implements Comparable<KeyBinding> {
    private static final Map<String, KeyBinding> KEYBIND_ARRAY = Maps.newHashMap();
    private static final Map<InputMappings.Input, KeyBinding> HASH = Maps.newHashMap();
    private static final Set<String> KEYBIND_SET = Sets.newHashSet();
-   private static final Map<String, Integer> CATEGORY_ORDER = Util.make(Maps.newHashMap(), (p_205215_0_) -> {
-      p_205215_0_.put("key.categories.movement", 1);
-      p_205215_0_.put("key.categories.gameplay", 2);
-      p_205215_0_.put("key.categories.inventory", 3);
-      p_205215_0_.put("key.categories.creative", 4);
-      p_205215_0_.put("key.categories.multiplayer", 5);
-      p_205215_0_.put("key.categories.ui", 6);
-      p_205215_0_.put("key.categories.misc", 7);
+   private static final Map<String, Integer> CATEGORY_ORDER = Util.make(Maps.newHashMap(), (p) -> {
+      p.put("key.categories.movement", 1); p.put("key.categories.gameplay", 2); p.put("key.categories.inventory", 3);
+      p.put("key.categories.creative", 4); p.put("key.categories.multiplayer", 5); p.put("key.categories.ui", 6); p.put("key.categories.misc", 7);
    });
    private final String keyDescription;
    private final InputMappings.Input keyCodeDefault;
@@ -39,115 +34,30 @@ public class KeyBinding implements Comparable<KeyBinding> {
       KeyBinding keybinding = HASH.get(key);
       if (keybinding != null) {
          ++keybinding.pressTime;
-         if (keybinding == UiUtilsMacroKeyBinding.KEY_BINDING) {
-            UiUtilsMacroKeyBinding.onPressed();
-         }
+         if (keybinding == UiUtilsMacroKeyBinding.KEY_BINDING) UiUtilsMacroKeyBinding.onPressed();
+         else UiUtilsMacroManager.handleKeyBinding(keybinding);
       }
    }
-
-   public static void setKeyBindState(InputMappings.Input key, boolean held) {
-      KeyBinding keybinding = HASH.get(key);
-      if (keybinding != null) {
-         keybinding.pressed = held;
-      }
-   }
-
-   public static void updateKeyBindState() {
-      for(KeyBinding keybinding : KEYBIND_ARRAY.values()) {
-         if (keybinding.keyCode.getType() == InputMappings.Type.KEYSYM && keybinding.keyCode.getKeyCode() != InputMappings.INPUT_INVALID.getKeyCode()) {
-            keybinding.pressed = InputMappings.isKeyDown(keybinding.keyCode.getKeyCode());
-         }
-      }
-   }
-
-   public static void unPressAllKeys() {
-      for(KeyBinding keybinding : KEYBIND_ARRAY.values()) {
-         keybinding.unpressKey();
-      }
-   }
-
-   public static void resetKeyBindingArrayAndHash() {
-      HASH.clear();
-      for(KeyBinding keybinding : KEYBIND_ARRAY.values()) {
-         HASH.put(keybinding.keyCode, keybinding);
-      }
-   }
-
-   public KeyBinding(String description, int keyCode, String category) {
-      this(description, InputMappings.Type.KEYSYM, keyCode, category);
-   }
-
-   public KeyBinding(String description, InputMappings.Type type, int code, String category) {
-      this.keyDescription = description;
-      this.keyCode = type.getOrMakeInput(code);
-      this.keyCodeDefault = this.keyCode;
-      this.keyCategory = category;
-      KEYBIND_ARRAY.put(description, this);
-      HASH.put(this.keyCode, this);
-      KEYBIND_SET.add(category);
-   }
-
-   public boolean isKeyDown() { return this.pressed; }
-   public String getKeyCategory() { return this.keyCategory; }
-
-   public boolean isPressed() {
-      if (this.pressTime == 0) {
-         return false;
-      } else {
-         --this.pressTime;
-         return true;
-      }
-   }
-
-   private void unpressKey() {
-      this.pressTime = 0;
-      this.pressed = false;
-   }
-
-   public String getKeyDescription() { return this.keyDescription; }
-   public InputMappings.Input getDefault() { return this.keyCodeDefault; }
-   public void bind(InputMappings.Input key) { this.keyCode = key; }
-
-   public int compareTo(KeyBinding p_compareTo_1_) {
-      return this.keyCategory.equals(p_compareTo_1_.keyCategory) ? I18n.format(this.keyDescription).compareTo(I18n.format(p_compareTo_1_.keyDescription)) : CATEGORY_ORDER.get(this.keyCategory).compareTo(CATEGORY_ORDER.get(p_compareTo_1_.keyCategory));
-   }
-
-   public static Supplier<String> getDisplayString(String key) {
-      KeyBinding keybinding = KEYBIND_ARRAY.get(key);
-      return keybinding == null ? () -> key : keybinding::getLocalizedName;
-   }
-
-   public boolean conflicts(KeyBinding binding) { return this.keyCode.equals(binding.keyCode); }
-   public boolean isInvalid() { return this.keyCode.equals(InputMappings.INPUT_INVALID); }
-
-   public boolean matchesKey(int keysym, int scancode) {
-      if (keysym == InputMappings.INPUT_INVALID.getKeyCode()) {
-         return this.keyCode.getType() == InputMappings.Type.SCANCODE && this.keyCode.getKeyCode() == scancode;
-      } else {
-         return this.keyCode.getType() == InputMappings.Type.KEYSYM && this.keyCode.getKeyCode() == keysym;
-      }
-   }
-
-   public boolean matchesMouseKey(int key) { return this.keyCode.getType() == InputMappings.Type.MOUSE && this.keyCode.getKeyCode() == key; }
-
-   public String getLocalizedName() {
-      String s = this.keyCode.getTranslationKey();
-      int i = this.keyCode.getKeyCode();
-      String s1 = null;
-      switch(this.keyCode.getType()) {
-      case KEYSYM:
-         s1 = InputMappings.func_216507_a(i);
-         break;
-      case SCANCODE:
-         s1 = InputMappings.func_216502_b(i);
-         break;
-      case MOUSE:
-         String s2 = I18n.format(s);
-         s1 = Objects.equals(s2, s) ? I18n.format(InputMappings.Type.MOUSE.func_216500_a(), i + 1) : s2;
-      }
-      return s1 == null ? I18n.format(s) : s1;
-   }
-
-   public boolean isDefault() { return this.keyCode.equals(this.keyCodeDefault); }
-   public String getTranslationKey() { return this.keyCode.getTranslationKey(); }
+   public static void setKeyBindState(InputMappings.Input key, boolean held) { KeyBinding k = HASH.get(key); if (k != null) k.pressed = held; }
+   public static void updateKeyBindState() { for (KeyBinding k : KEYBIND_ARRAY.values()) if (k.keyCode.getType() == InputMappings.Type.KEYSYM && k.keyCode.getKeyCode() != InputMappings.INPUT_INVALID.getKeyCode()) k.pressed = InputMappings.isKeyDown(k.keyCode.getKeyCode()); }
+   public static void unPressAllKeys() { for (KeyBinding k : KEYBIND_ARRAY.values()) k.unpressKey(); }
+   public static void resetKeyBindingArrayAndHash() { HASH.clear(); for (KeyBinding k : KEYBIND_ARRAY.values()) HASH.put(k.keyCode, k); }
+   public KeyBinding(String description, int keyCode, String category) { this(description, InputMappings.Type.KEYSYM, keyCode, category); }
+   public KeyBinding(String description, InputMappings.Type type, int code, String category) { keyDescription=description; keyCode=type.getOrMakeInput(code); keyCodeDefault=keyCode; keyCategory=category; KEYBIND_ARRAY.put(description,this); HASH.put(this.keyCode,this); KEYBIND_SET.add(category); }
+   public boolean isKeyDown() { return pressed; }
+   public String getKeyCategory() { return keyCategory; }
+   public boolean isPressed() { if (pressTime == 0) return false; --pressTime; return true; }
+   private void unpressKey() { pressTime=0; pressed=false; }
+   public String getKeyDescription() { return keyDescription; }
+   public InputMappings.Input getDefault() { return keyCodeDefault; }
+   public void bind(InputMappings.Input key) { keyCode=key; }
+   public int compareTo(KeyBinding other) { return keyCategory.equals(other.keyCategory) ? I18n.format(keyDescription).compareTo(I18n.format(other.keyDescription)) : CATEGORY_ORDER.get(keyCategory).compareTo(CATEGORY_ORDER.get(other.keyCategory)); }
+   public static Supplier<String> getDisplayString(String key) { KeyBinding k=KEYBIND_ARRAY.get(key); return k==null ? () -> key : k::getLocalizedName; }
+   public boolean conflicts(KeyBinding binding) { return keyCode.equals(binding.keyCode); }
+   public boolean isInvalid() { return keyCode.equals(InputMappings.INPUT_INVALID); }
+   public boolean matchesKey(int keysym,int scancode) { return keysym==InputMappings.INPUT_INVALID.getKeyCode() ? keyCode.getType()==InputMappings.Type.SCANCODE&&keyCode.getKeyCode()==scancode : keyCode.getType()==InputMappings.Type.KEYSYM&&keyCode.getKeyCode()==keysym; }
+   public boolean matchesMouseKey(int key) { return keyCode.getType()==InputMappings.Type.MOUSE&&keyCode.getKeyCode()==key; }
+   public String getLocalizedName() { String s=keyCode.getTranslationKey(); int i=keyCode.getKeyCode(); String s1=null; switch(keyCode.getType()) { case KEYSYM: s1=InputMappings.func_216507_a(i); break; case SCANCODE: s1=InputMappings.func_216502_b(i); break; case MOUSE: String s2=I18n.format(s); s1=Objects.equals(s2,s)?I18n.format(InputMappings.Type.MOUSE.func_216500_a(),i+1):s2; } return s1==null?I18n.format(s):s1; }
+   public boolean isDefault() { return keyCode.equals(keyCodeDefault); }
+   public String getTranslationKey() { return keyCode.getTranslationKey(); }
 }
