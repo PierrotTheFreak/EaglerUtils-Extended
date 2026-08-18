7ba6e7fa810c1fb2a7b62cbe6e4e2f1342ffb485
PierrotTheFreak <pierrotthefreak0@gmail.com>
2026-08-18 12:04:16 -0400

    fix: modified src/main/java/net/minecraft/client/gui/uiutils/UiUtils.java

--- src/main/java/net/minecraft/client/gui/uiutils/UiUtils.java
+++ src/main/java/net/minecraft/client/gui/uiutils/UiUtils.java
@@ -35,7 +35,7 @@ public class UiUtils {
     private static final Set<Class<?>> delayedPacketTypes =
             new LinkedHashSet<>();
 
-    private UiUtilsPacketManager() {
+    private UiUtils() {
     }
 
     /*
